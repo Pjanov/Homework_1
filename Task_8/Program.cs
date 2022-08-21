347127865613214831babe80bbc7f06746f9f873
Андрей Пьянов <109807270+Pjanov@users.noreply.github.com>
2022-08-21 19:09:00 +0300

    Update Program.cs

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -3,40 +3,12 @@
 
 5 -> 2, 4
 8 -> 2, 4, 6, 8 */
-
-
-
-int num1 = 5;
-int i1 = 2;
-Console.WriteLine("Вывод четных чисел от 1 до " + (num1) + ":");
-
-while (i1 < num1-1)
-{
-    Console.Write((i1) + ", ");
-    i1 = i1 + 2;
-}
-Console.WriteLine((i1) + ". ");
-
-
-
-
-int num2 = 8;
-int i2 = 2;
-Console.WriteLine("Вывод четных чисел от 1 до " + (num2) + ":");
-
-while (i2 < num2-1)
-{
-    Console.Write((i2) + ", ");
-    i2 = i2 + 2;
-}
-Console.WriteLine((i2) + ". ");
-
-
-/*              //Всамой программе применил  операторы if и else, что бы не вводили меньше единицы !
+*/
+             //Всамой программе применил  операторы if и else, что бы не вводили меньше единицы !
 Console.Write("Введите положительное целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int i = 2;
-Console.WriteLine("Вывод четных чисел от 1 до " + (num) + ":");
+Console.WriteLine($"Вывод четных чисел от 1 до {num}: ");
 if (num <= 1)
 {
     Console.WriteLine("Число должно быть БОЛЬШЕ единийы !!!");
@@ -45,9 +17,8 @@ else
 {
    while (i < num - 1)
 {
-    Console.Write((i) + ", ");
+    Console.Write($"{i}, ");
     i = i + 2;
 }
-Console.Write((i) + ". "); 
+Console.Write($"{i}. "); 
 }
-*/
