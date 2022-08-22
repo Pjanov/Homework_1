35f70ad0dac5ea0e369eb447f826f7f97184f874
Андрей <pjanov84@mail.ru>
2022-08-22 18:25:08 +0300

    fixed the code

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -9,11 +9,7 @@ Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третие число: ");
 int c = Convert.ToInt32(Console.ReadLine());
-
 int max = a;
-
-if (a > max) max = a;
 if (b > max) max = b;
 if (c > max) max = c;
-
 Console.WriteLine($"Максимальное число: max = {max}");
